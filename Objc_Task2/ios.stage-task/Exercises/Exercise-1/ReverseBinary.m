b2126bfbfd361bb558ecfc1b09f3fe4aaa59428b
romax114 <70773814+romax114@users.noreply.github.com>
2021-05-18 01:40:35 +0300

    feat: implement solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,27 @@
+
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+    NSString *hexString = @"";
+    NSMutableString *reversedString = [[NSMutableString alloc] init];
+    
+    
+    for (int i = 0; i < 8; i++) {
+        hexString = [NSString stringWithFormat:@"%i%@", n & (1<<i) ? 1 : 0, hexString];
+        
+        
+    }
+
+    NSInteger charIndex = [hexString length];
+    
+    while (charIndex > 0) {
+        charIndex --;
+        NSRange subStringRange = NSMakeRange(charIndex, 1);
+        [reversedString appendString:[hexString substringWithRange:subStringRange]];
+    }
+    
+    UInt8 finalNumber = strtol([reversedString UTF8String], nil, 2);
+    
+    return finalNumber;
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,27 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    if([array count] == 0 || array == nil || ![array[0] isKindOfClass:[NSArray class]]){
+        return @[];
+    }
+    
+    NSMutableArray *numbers = [NSMutableArray arrayWithArray:@[]];
+    NSMutableArray *strings = [NSMutableArray arrayWithArray:@[]];
+    
+    for (id item in array) {
+        for (id el in item) {
+            if ([item isKindOfClass:NSString.class]) [strings addObject:el];
+            else [numbers addObject:el];
+        }
+    }
+    
+    [numbers sortUsingSelector:@selector(compare:)];
+    [strings sortUsingSelector:@selector(compare:)];
+    
+    if (numbers.count == 0) return strings;
+    if (strings.count == 0) return numbers;
+        
+    return [NSArray arrayWithObjects: numbers, strings, nil];
 }
 
 @end
